b4185b63dea8ff15ab9a2020096118b7516e6792
laismullerrr <lais.muller@hotmail.com>
2023-09-02 21:46:29 -0300

    feat: :zap: mudança para loop for

--- main.js
+++ main.js
@@ -1,16 +1,21 @@
-function tocaSom(idElementoAudio) {
+function tocaSom (idElementoAudio) {
     document.querySelector(idElementoAudio).play();
 }
 
-const listaDeTeclas = document.querySelectorAll('.tecla')
-let contador = 0;
+const listaDeTeclas = document.querySelectorAll('.tecla');
 
-while (contador < listaDeTeclas.length ){
-    listaDeTeclas[contador].onclick = tocaSom;
-    contador = contador +1;
-    console.log(contador);
+//para
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`; //template string
+
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    }
+
 }
-
 
 
  //document.querySelector('.tecla_pom').onclick = tocaSomPom;
