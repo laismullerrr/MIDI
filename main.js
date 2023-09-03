a6b1cbcbb347be3378c545b180ac66e921105521
laismullerrr <lais.muller@hotmail.com>
2023-09-02 21:35:02 -0300

    feat: :zap: mudança para loop while

--- main.js
+++ main.js
@@ -1,52 +1,65 @@
-function tocaSomPom () {
-    document.querySelector('#som_tecla_pom').play();
+function tocaSom(idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
 }
-document.querySelector('.tecla_pom').onclick = tocaSomPom;
 
+const listaDeTeclas = document.querySelectorAll('.tecla')
+let contador = 0;
 
-function tocaSomClap () {
-    document.querySelector('#som_tecla_clap').play();
+while (contador < listaDeTeclas.length ){
+    listaDeTeclas[contador].onclick = tocaSom;
+    contador = contador +1;
+    console.log(contador);
 }
-document.querySelector('.tecla_clap').onclick = tocaSomClap;
 
 
-function tocaSomTim () {
-    document.querySelector('#som_tecla_tim').play();
-}
-document.querySelector('.tecla_tim').onclick = tocaSomTim;
+
+ //document.querySelector('.tecla_pom').onclick = tocaSomPom;
 
 
-function tocaSomPuff () {
-    document.querySelector('#som_tecla_puff').play();
-}
-document.querySelector('.tecla_puff').onclick = tocaSomPuff;
+// function tocaSomClap () {
+//     document.querySelector('#som_tecla_clap').play();
+// }
+// document.querySelector('.tecla_clap').onclick = tocaSomClap;
 
 
-function tocaSomSplash () {
-    document.querySelector('#som_tecla_splash').play();
-}
-document.querySelector('.tecla_splash').onclick = tocaSomSplash;
+// function tocaSomTim () {
+//     document.querySelector('#som_tecla_tim').play();
+// }
+// document.querySelector('.tecla_tim').onclick = tocaSomTim;
 
 
-function tocaSomToim () {
-    document.querySelector('#som_tecla_toim').play();
-}
-document.querySelector('.tecla_toim').onclick = tocaSomToim;
+// function tocaSomPuff () {
+//     document.querySelector('#som_tecla_puff').play();
+// }
+// document.querySelector('.tecla_puff').onclick = tocaSomPuff;
 
 
-function tocaSomPsh () {
-    document.querySelector('#som_tecla_psh').play();
-}
-document.querySelector('.tecla_psh').onclick = tocaSomPsh;
+// function tocaSomSplash () {
+//     document.querySelector('#som_tecla_splash').play();
+// }
+// document.querySelector('.tecla_splash').onclick = tocaSomSplash;
 
 
-function tocaSomTic () {
-    document.querySelector('#som_tecla_tic').play();
-}
-document.querySelector('.tecla_tic').onclick = tocaSomTic;
+// function tocaSomToim () {
+//     document.querySelector('#som_tecla_toim').play();
+// }
+// document.querySelector('.tecla_toim').onclick = tocaSomToim;
 
 
-function tocaSomTom () {
-    document.querySelector('#som_tecla_tom').play();
-}
-document.querySelector('.tecla_tom').onclick = tocaSomTom;
+// function tocaSomPsh () {
+//     document.querySelector('#som_tecla_psh').play();
+// }
+// document.querySelector('.tecla_psh').onclick = tocaSomPsh;
+
+
+// function tocaSomTic () {
+//     document.querySelector('#som_tecla_tic').play();
+// }
+// document.querySelector('.tecla_tic').onclick = tocaSomTic;
+
+
+// function tocaSomTom () {
+//     document.querySelector('#som_tecla_tom').play();
+// }
+// document.querySelector('.tecla_tom').onclick = tocaSomTom;
+
